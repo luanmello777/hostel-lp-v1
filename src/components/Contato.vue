166e4780520d5ef0e1c5fc6159b0120230645fcf
Luan Mello <close8821@gmail.com>
2024-09-24 17:20:55 -0300

    Feat | Att

--- src/components/Contato.vue
+++ src/components/Contato.vue
@@ -1,17 +1,39 @@
 <template>
     <div class="bg-secondary pt-sm-16">
-        <div class="d-flex pt-16 pb-16 px-2 mt-xl-16" align="center" justify="center" id="contato">
+        <div class="d-md-flex pt-16 pb-16 px-2 mt-xl-16" align="center" justify="center" id="contato">
 
-            <div class="w-100 d-sm-none">
-                <v-img src="/contato.jpg" height="270" cover />
-            </div>
-
-            <div class="w-100 d-none d-sm-block d-lg-none">
-                <v-img src="/contato.jpg" />
-            </div>
-
-            <div class="w-100 d-none d-lg-block">
-                <v-img src="/contato.jpg" height="700" />
+            <div class="w-100" align="center" justify="center">
+                <v-form ref="form" action="https://formsubmit.co/soulhostelnatal@gmail.com" method="POST" @submit.prevent="validateForm">
+                    <v-container class="mt-lg-16 pt-lg-16 mb-16 mb-md-0">
+                      <v-row>
+                        <v-col cols="12" sm="12" md="12" class="rounded-xl">
+                          <v-text-field v-model="nome" name="nome" label="Nome" color="blue" hint="Informe seu Nome" required></v-text-field>
+                        </v-col>
+                
+                        <v-col cols="12" sm="12" md="12">
+                          <v-text-field v-model="email" name="email" label="E-mail" placeholder="email@email.com.br" hint="Informe seu E-mail" color="blue" required></v-text-field>
+                        </v-col>
+                
+                        <v-col cols="12" sm="12" md="12">
+                          <v-text-field v-model="whatsapp" name="whatsapp" label="WhatsApp" color="blue" placeholder="ddd + número" hint="Informe seu WhatsApp" required></v-text-field>
+                        </v-col>
+                
+                        <v-col cols="12" sm="12" md="12">
+                          <v-text-field v-model="mensagem" name="mensagem" label="Consultar Hospedagem" color="blue" hint="Informe sua Mensagem" required></v-text-field>
+                        </v-col>
+                      </v-row>
+                
+                      <input type="hidden" name="_next" value="https://www.booking.com/hotel/br/soul-hostel-natal.pt-br.html?label=gen173nr-1BCAEoggI46AdIM1gEaCCIAQGYAS24ARfIAQzYAQHoAQGIAgGoAgO4AuKh4bMGwAIB0gIkNjg1OTVkMjMtMDU1ZC00MTYzLWJkZGYtYWM4M2Y4ZTRjNTJm2AIF4AIB&sid=b7317855976ce3f9986bc94c9ec1d674&aid=304142&ucfs=1&arphpl=1&dest_id=5718445&dest_type=hotel&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=1&hapos=1&sr_order=popularity&srpvid=f38475b479800133&srepoch=1719161077&from_sustainable_property_sr=1&from=searchresults">
+                      <input type="hidden" name="_captcha" value="false">
+                      <input type="hidden" name="_subject" value="Novo envio de formulário!">
+                
+                      <div align="center">
+                        <v-btn type="submit" class="bg-blue w-75 w-lg-50 mt-md-8 rounded-pill" size="large">
+                          <h3 class="text-white">BOOKING.COM</h3>
+                        </v-btn>
+                      </div>
+                    </v-container>
+                  </v-form>
             </div>
 
 
@@ -28,13 +50,13 @@
                 </h3>
 
                 <div class="mt-3 mt-sm-5 mt-lg-16 w-100">
-                    <v-btn class="bg-green rounded-pill w-75 w-sm-50 d-lg-none" href=" https://wa.me/558499900268"
+                    <v-btn class="bg-green rounded-pill w-75 w-sm-50 d-lg-none" href="https://api.whatsapp.com/send/?phone=558499900268&text=Ol%C3%A1%21%20Estou%20vindo%20do%20Google%20e%20gostaria%20de%20valores%20e%20op%C3%A7%C3%B5es%20de%20reservas%21"
                         target="_blank">
                         <h4>Whatsapp</h4>
                     </v-btn>
 
                     <v-btn class="bg-green rounded-pill w-50 d-none d-lg-block" size="x-large" height="60"
-                        href=" https://wa.me/558499900268" target="_blank">
+                        href="https://api.whatsapp.com/send/?phone=558499900268&text=Ol%C3%A1%21%20Estou%20vindo%20do%20Google%20e%20gostaria%20de%20valores%20e%20op%C3%A7%C3%B5es%20de%20reservas%21" target="_blank">
                         <h4 class="pt-5">Whatsapp</h4>
                     </v-btn>
                     <br>
@@ -50,20 +72,7 @@
                     </v-btn>
                     <br>
 
-                    <v-btn class="bg-blue rounded-pill w-75 w-sm-50 d-lg-none"
-                        href="https://www.booking.com/hotel/br/soul-hostel-natal.pt-br.html?label=gen173nr-1BCAEoggI46AdIM1gEaCCIAQGYAS24ARfIAQzYAQHoAQGIAgGoAgO4AuKh4bMGwAIB0gIkNjg1OTVkMjMtMDU1ZC00MTYzLWJkZGYtYWM4M2Y4ZTRjNTJm2AIF4AIB&sid=b7317855976ce3f9986bc94c9ec1d674&aid=304142&ucfs=1&arphpl=1&dest_id=5718445&dest_type=hotel&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=1&hapos=1&sr_order=popularity&srpvid=f38475b479800133&srepoch=1719161077&from_sustainable_property_sr=1&from=searchresults"
-                        target="_blank">
-                        <h4>Booking.com</h4>
-                    </v-btn>
-
-                    <v-btn class="bg-blue rounded-pill w-50 d-none d-lg-block" size="x-large" height="60"
-                        href="https://www.booking.com/hotel/br/soul-hostel-natal.pt-br.html?label=gen173nr-1BCAEoggI46AdIM1gEaCCIAQGYAS24ARfIAQzYAQHoAQGIAgGoAgO4AuKh4bMGwAIB0gIkNjg1OTVkMjMtMDU1ZC00MTYzLWJkZGYtYWM4M2Y4ZTRjNTJm2AIF4AIB&sid=b7317855976ce3f9986bc94c9ec1d674&aid=304142&ucfs=1&arphpl=1&dest_id=5718445&dest_type=hotel&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=1&hapos=1&sr_order=popularity&srpvid=f38475b479800133&srepoch=1719161077&from_sustainable_property_sr=1&from=searchresults"
-                        target="_blank">
-                        <h4 class="pt-5">Booking.com</h4>
-                    </v-btn>
-                    <br>
-
-                    <v-btn class="bg-black rounded-pill mt-3 w-75 w-sm-50 d-lg-none"
+                    <v-btn class="bg-black rounded-pill w-75 w-sm-50 d-lg-none"
                         href="mailto:soulhostelnatal@gmail.com" target="_blank">
                         <h4>E-mail</h4>
                     </v-btn>
@@ -83,3 +92,22 @@
     font-family: "Berkshire Swash", serif;
 }
 </style>
+
+<script setup>
+import { ref } from 'vue';
+
+const nome = ref('');
+const email = ref('');
+const whatsapp = ref('');
+const mensagem = ref('');
+
+const form = ref(null);
+
+const validateForm = () => {
+  if (!nome.value || !email.value || !whatsapp.value || !mensagem.value) {
+    alert('Por favor, preencha todos os campos antes de enviar.');
+  } else {
+    form.value.submit();
+  }
+};
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,52 +5,57 @@
       <div class="background w-50 h-100 pl-2 pl-sm-8 pt-sm-16" style="line-height: 20px;" justify="center" align="left">
 
         <div class="pt-lg-16 mt-lg-16 pl-lg-10">
-        <h4 class="font text-white font-weight-light pt-16 mt-sm-16 text-sm-h4 text-md-h3" style="line-height: 20px;"
-          align="left">
-          <span class="text-primary">FÉRIAS</span> EM
-        </h4>
+          <h4 class="font text-white font-weight-light pt-16 mt-sm-16 text-sm-h4 text-md-h3" style="line-height: 20px;"
+            align="left">
+            <span class="text-primary">FÉRIAS</span> EM
+          </h4>
 
 
 
-        <h1 class="font text-white font-weight-light text-sm-h2 text-md-h1 text-lg-h1">NATAL-RN
-        </h1>
+          <h1 class="font text-white font-weight-light text-sm-h2 text-md-h1 text-lg-h1">NATAL-RN
+          </h1>
 
-        <h6 class="text-white font-weight-light d-sm-none" style="font-size: 5px; line-height: 20px;">HOSPEDAGEM
-          <span class="font-weight-bold text-primary">ACONCHEGANTE</span> COM VALORES <span
-            class="font-weight-bold text-primary">PROMOCIONAIS</span>
-        </h6>
+          <h6 class="text-white font-weight-light d-sm-none" style="font-size: 5px; line-height: 20px;">HOSPEDAGEM
+            <span class="font-weight-bold text-primary">ACONCHEGANTE</span> COM VALORES <span
+              class="font-weight-bold text-primary">PROMOCIONAIS</span>
+          </h6>
 
-        <h6 class="text-white font-weight-light d-none d-sm-block d-md-none"
-          style="font-size: 9.2px; line-height: 10px;">HOSPEDAGEM
-          <span class="font-weight-bold text-primary">ACONCHEGANTE</span> COM VALORES <span
-            class="font-weight-bold text-primary">PROMOCIONAIS</span>
-        </h6>
+          <h6 class="text-white font-weight-light d-none d-sm-block d-md-none"
+            style="font-size: 9.2px; line-height: 10px;">HOSPEDAGEM
+            <span class="font-weight-bold text-primary">ACONCHEGANTE</span> COM VALORES <span
+              class="font-weight-bold text-primary">PROMOCIONAIS</span>
+          </h6>
 
-        <h6 class="text-white font-weight-light d-none d-md-block" style="font-size: 14.5px; line-height: 13px;">
-          HOSPEDAGEM
-          <span class="font-weight-bold text-primary">ACONCHEGANTE</span> COM VALORES <span
-            class="font-weight-bold text-primary">PROMOCIONAIS</span>
-        </h6>
+          <h6 class="text-white font-weight-light d-none d-md-block" style="font-size: 14.5px; line-height: 13px;">
+            HOSPEDAGEM
+            <span class="font-weight-bold text-primary">ACONCHEGANTE</span> COM VALORES <span
+              class="font-weight-bold text-primary">PROMOCIONAIS</span>
+          </h6>
 
 
-        <div>
-          <v-btn class="bg-primary rounded-pill w-50 w-sm-75 mt-1 mt-sm-5 d-md-none" size="small"  href=" https://wa.me/558499900268" target="_blank">RESERVAS</v-btn>
+          <div>
+            <v-btn class="bg-primary rounded-pill w-50 w-sm-75 mt-1 mt-sm-5 d-md-none" size="small"
+              href=" https://wa.me/558499900268" target="_blank">RESERVAS</v-btn>
 
-          <v-btn class="bg-primary rounded-pill w-50 w-sm-75 mt-1 mt-sm-5 d-none d-md-block d-lg-none"
-            size="large"  href=" https://wa.me/558499900268" target="_blank"><h4 class="pt-3">CONSULTAR RESERVAS</h4></v-btn>
+            <v-btn class="bg-primary rounded-pill w-50 w-sm-75 mt-1 mt-sm-5 d-none d-md-block d-lg-none" size="large"
+              href=" https://wa.me/558499900268" target="_blank">
+              <h4 class="pt-3">CONSULTAR RESERVAS</h4>
+            </v-btn>
 
-            <v-btn class="bg-primary rounded-pill w-50 mt-8 d-none d-lg-block"
-            size="x-large"  href=" https://wa.me/558499900268" target="_blank"><h4 class="pt-4">CONSULTAR RESERVAS</h4></v-btn>
+            <v-btn class="bg-primary rounded-pill w-50 mt-8 d-none d-lg-block" size="x-large"
+              href=" https://wa.me/558499900268" target="_blank">
+              <h4 class="pt-4">CONSULTAR RESERVAS</h4>
+            </v-btn>
+          </div>
+
+
         </div>
-
-
       </div>
-  </div>
 
 
 
 
-  </v-img>
+    </v-img>
   </div>
 
 </template>
